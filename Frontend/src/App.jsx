1b1ce424afa0b7f30ac770bccc22d6ccfd753892
RajaXSharma <rajasharma844891@gmail.com>
2024-09-21 19:47:57 +0530

    connected sockets of frontend and server

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,103 @@
-import { useEffect } from "react";
+// import { useEffect, useState } from "react";
+// import "./App.css";
+// import io from "socket.io-client";
+// function App() {
+//   const [message, setMessage] = useState("");
+
+//   const socket = io("http://localhost:4000/");
+//   useEffect(() => {
+//     socket.on("connect", () => {
+//       console.log("connected");
+//     });
+
+//     socket.on("disconnect", () => [console.log("disconnected")]);
+
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, [socket]);
+
+//   const handleForm = (e) => {
+//     e.preventDefault();
+//     if (message) {
+//       socket.emit("chat message", message);
+//       setMessage("");
+//     }
+//   };
+
+//   return (
+//     <>
+//       <form onSubmit={handleForm}>
+//         <input
+//           type="text"
+//           onChange={(e) => setMessage(e.target.value)}
+//           value={message}
+//           placeholder="enter message"
+//         />
+//         <button type="submit">send</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
+
 function App() {
-  const socket = io("http://localhost:4000/");
+  const [message, setMessage] = useState("");
+  const [socket, setSocket] = useState(null);
+  const [chat, setChat] = useState([]);
 
   useEffect(() => {
-    socket.on("connect", () => {
+    const newSocket = io("http://localhost:4000/");
+    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
       console.log("connected");
     });
 
-    socket.on("disconnect", () => [console.log("disconnected")]);
+    newSocket.on("chat message", (msg) => {
+      setChat((prevChat) => [...prevChat, msg]);
+    });
+
+    newSocket.on("disconnect", () => {
+      console.log("disconnected");
+    });
 
     return () => {
-      socket.disconnect();
+      newSocket.disconnect();
     };
-  }, [socket]);
+  }, []);
 
-  return <></>;
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (message && socket) {
+      socket.emit("chat message", message);
+      setMessage("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder="enter message"
+        />
+        <button type="submit">send</button>
+      </form>
+      <div className="chat">
+        {chat.map((chatMessage, index) => (
+          <p key={index}>{chatMessage}</p>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     methods: ["GET", "POST"],
   },
 });
@@ -20,6 +20,15 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    // console.log("message :", msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
 });
 
 server.listen(4000, () => {
